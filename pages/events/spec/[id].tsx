811ed535ea1b8665ae6a4c462a05175e9cf59161
Jacob Fernandes <59456400+proballstar@users.noreply.github.com>
2022-11-06 01:47:44 -0800

    Adding correct maps functionality

--- pages/events/spec/[id].tsx
+++ pages/events/spec/[id].tsx
@@ -51,10 +51,6 @@ export default function SpecificEvent(props) {
               </Text>
             </Container>
             <Grid.Col py={20}>
-              <Image
-                src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg"
-                alt="London"
-              />
               <Maps
                 address={props.name}
                 latitude={props.latitude}
